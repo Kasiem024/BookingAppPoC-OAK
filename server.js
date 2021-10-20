0009ec70fb00012151491311602736c021c19d22
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-20 12:27:31 +0200

    Co-authored-by: Oscar <ogus02@users.noreply.github.com> created booking site and experimenting with sheets api

--- server.js
+++ server.js
@@ -19,8 +19,32 @@ const authentication = async() => {
     const auth = new google.auth.Google.Auth({
         keyFile: "credentials.json",
         scopes: "https://www.googleapis.com/auth/spreadsheets"
-    })
+    });
+
+    const client = await auth.getClient();
+
+    const sheets = google.sheets({
+        version: 'v4',
+        auth: client
+    });
+    return { sheets }
 }
+
+const id = '1zlESD_q8esLsr3rgQi5bh6lWBKinTXApBUm8k3ijyT4';
+
+app.get('/', async(req, res) => {
+    try {
+        const { sheets } = await authentication();
+        const response = await sheets.spreadsheets.values.get({
+            spreadsheetId: id,
+            range: 'Sheet1',
+        })
+        res.send(response.data)
+    } catch (e) {
+        console.log(e);
+        res.status(500).send();
+    }
+});
 
 app.listen(port);
 
