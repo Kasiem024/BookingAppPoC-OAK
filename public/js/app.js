4c4fc3a06578d79f0ff7770b0859e6968b50e0e8
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-20 18:45:43 +0200

    created user list in json file, XHR in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,17 @@
 
 console.log('app.js is alive!')
 
+let dataURL = '../data/users.json';
+let req = new XMLHttpRequest();
+req.open('GET', dataURL);
+req.responseType = 'json';
+req.send();
+
+req.onload = () => {
+    const data = req.response;
+    console.log(data.users[2].name);
+}
+
 const Login = () => {
     let inputName = document.getElementById('name').value;
 
