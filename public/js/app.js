ca83bec36de56781405b0e1d9c6cd921161183dd
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-20 10:24:12 +0200

    added fucntioning log in button

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log('app.js is alive!')
+
+const Login = () => {
+    let inputName = document.getElementById('name').value;
+
+    if (inputName == 'Kalle') {
+        // window.location.href = 'home';
+        console.log('Logged In')
+    } else {
+        console.log('That is not a valid account');
+    }
+}
