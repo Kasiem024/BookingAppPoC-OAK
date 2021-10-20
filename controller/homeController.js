64164a1c1f80a4ec559e017762414d40d159a467
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-20 13:40:24 +0200

    gave up on google sheets api, created json file

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,42 +1,5 @@
 'use strict';
 
 exports.index = (req, res) => {
-    const express = require('express');
-    const app = express();
-
-    const { google } = require('googleapis')
-
-    app.use(express.json());
-
-    const authentication = async() => {
-        const auth = new google.auth.Google.Auth({
-            keyFile: "credentials.json",
-            scopes: "https://www.googleapis.com/auth/spreadsheets"
-        });
-
-        const client = await auth.getClient();
-
-        const sheets = google.sheets({
-            version: 'v4',
-            auth: client
-        });
-        return { sheets }
-    }
-
-    const id = '1zlESD_q8esLsr3rgQi5bh6lWBKinTXApBUm8k3ijyT4';
-
-    app.get('/', async(req, res) => {
-        try {
-            const { sheets } = await authentication();
-            const response = await sheets.spreadsheets.values.get({
-                spreadsheetId: id,
-                range: 'Sheet1',
-            })
-            res.send(response.data)
-        } catch (e) {
-            console.log(e);
-            res.status(500).send();
-        }
-    });
     res.sendfile('public/index.html');
 };
--- server.js
+++ server.js
@@ -3,48 +3,13 @@
 const express = require('express');
 const app = express();
 
-const { google } = require('googleapis')
-
 const indexRouter = require('./routes/indexRoute');
 
 const port = process.env.PORT || 8042;
 
-app.use(express.json());
-
 app.use(express.static('public'));
 
 app.use('/', indexRouter);
-
-const authentication = async() => {
-    const auth = new google.auth.Google.Auth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets"
-    });
-
-    const client = await auth.getClient();
-
-    const sheets = google.sheets({
-        version: 'v4',
-        auth: client
-    });
-    return { sheets }
-}
-
-const id = '1zlESD_q8esLsr3rgQi5bh6lWBKinTXApBUm8k3ijyT4';
-
-app.get('/', async(req, res) => {
-    try {
-        const { sheets } = await authentication();
-        const response = await sheets.spreadsheets.values.get({
-            spreadsheetId: id,
-            range: 'Sheet1',
-        })
-        res.send(response.data)
-    } catch (e) {
-        console.log(e);
-        res.status(500).send();
-    }
-});
 
 app.listen(port);
 
