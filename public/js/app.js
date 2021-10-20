55bfe47be736a36e8e191e74208cc6719cd59023
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-20 11:28:12 +0200

    experimenting with google sheets api

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,7 @@ console.log('app.js is alive!')
 const Login = () => {
     let inputName = document.getElementById('name').value;
 
-    if (inputName == 'Kalle') {
-        // window.location.href = 'home';
+    if (inputName == 'kalle') {
         console.log('Logged In')
     } else {
         console.log('That is not a valid account');
